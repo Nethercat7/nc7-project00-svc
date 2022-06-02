0884af43f5b673eeee15ceb9dfddc197fb541c59
Nethercat7 <1209322690@qq.com>
2022-06-03 04:53:53 +0800

    initial:初始化框架

---
+++ src/main/java/com/nc7/project00/Nc7Project00SvcApplication.java
@@ -0,0 +1,13 @@
+package com.nc7.project00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nc7Project00SvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nc7Project00SvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nc7/project00/Nc7Project00SvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc7.project00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nc7Project00SvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
